2108745133aac2ad8e621149aa05921736f2a117
Brishav Mondal <100032532+BrishavMondal@users.noreply.github.com>
2024-11-24 16:39:18 +0600

    Add files via upload

--- Web Tech MID lab exam/Login_Check.php
+++ Web Tech MID lab exam/Login_Check.php
@@ -1,37 +1,55 @@
 <?php
     session_start();
+    $username;
+    $password;
+    $name;
+    $id;
+    $email;
+    $date;
+    $bg;
+
+
     if(isset($_REQUEST['register']))
     {
         header('location:form.html');
-    }
-    elseif(isset($_REQUEST['submit'])){
-        //print_r($_GET);
-        //echo "Test";
-        //var_dump($_GET);
-
-        // $abc = 10;
-
-        // function sum(){
-        //     //$GLOBALS['abc'];
-        //     global $abc;
-        // }
-
+    
+    if(isset($_REQUEST['submitf'])){
+       
         $username = trim($_POST['username']);
         $password = trim($_REQUEST['password']);
-        echo "your username is: ".$username;
-        echo "your username is: {$username}";
-
-        if($username == null || empty($password)){
-            echo "Null username/password!";
-
-        }else if ($username == $password) {
-            //echo "valid user!";
-            $_SESSION['status'] = true;
-            
-        }else{
-            echo "invalid user!";
+        $name=trim($_REQUEST['name']);
+        $id=trim($_REQUEST['id']);
+        $email=trim($_REQUEST['email']);
+        $date=trim($_REQUEST['date']);
+        $bg=trim($_REQUEST['bg']);
         }
-    }else{
+    }
+
+
+
+
+    elseif(isset($_REQUEST['submit']))
+    {
+        $usernamel = trim($_POST['usernamel']);
+        $passwordl = trim($_REQUEST['passwordl']);
+        if($usernamel == null || empty($passwordl))
+        {header('location: login.html');}
+
+        elseif(strcmp($usernamel,$username)==0 && strcmp($password,$passwordl)==0){
+          echo "your username is: {$username}";
+          echo "Password is     : {$password}";
+          echo "Your Name       : {$name}";
+          echo "Your Id         : {$id}";
+          echo "Your email      : {$email}";
+          echo "Your DOB        : {$date}";
+          echo "Your Blood Group: {$bg}";
+        }
+    }
+
+    
+   else
+
+    {
         header('location: login.html');
     }
 
